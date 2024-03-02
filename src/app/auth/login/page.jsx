04070a66c93d7d2ac1f791014bc5b75d67a06c71
Jhon Torres <38541491+jhon-torres@users.noreply.github.com>
2024-03-01 21:41:26 -0500

    user registration

---
+++ src/app/auth/login/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+        <form className="" action="" method="">
+            <input
+                type="text"
+                id="id"
+                name="name"
+                placeholder="placeholder"
+                className="w-[300px] border border-slate-200 rounded-lg py-3 px-5 outline-none	bg-transparent"
+            />
+        </form>
+    </div>
+  )
+}
+
+export default LoginPage
